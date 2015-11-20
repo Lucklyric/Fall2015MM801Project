89377166b3e3cc84d8c78a4d653d9644d58eae01
Hongzu Li <hongzu@ualberta.ca>
2015-11-20 16:07:18 -0700

    add myo controller

---
+++ MyoCookbook/MyoCookbook/MyoController.h
@@ -0,0 +1,13 @@
+//
+//  MyoController.h
+//  MyoCookbook
+//
+//  Created by Hongzu Li on 2015-11-20.
+//  Copyright © 2015 Alvin Sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyoController : NSObject
+
+@end
---
+++ MyoCookbook/MyoCookbook/MyoController.m
@@ -0,0 +1,13 @@
+//
+//  MyoController.m
+//  MyoCookbook
+//
+//  Created by Hongzu Li on 2015-11-20.
+//  Copyright © 2015 Alvin Sun. All rights reserved.
+//
+
+#import "MyoController.h"
+
+@implementation MyoController
+
+@end
